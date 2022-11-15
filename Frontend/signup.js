66bbebb7defbfc9357113901e163e5a179ca827e
Muzahir7 <32150314+Muzahir7@users.noreply.github.com>
2022-11-15 15:40:27 -0600

    Signup Page
    signup page integrated with home page and login page

---
+++ Frontend/signup.js
@@ -0,0 +1,12 @@
+// const user = {
+//   fname: "Muzahir",
+//   lname: "Baloch",
+//   address: "Dallas, TX",
+//   username: "Muzi",
+//   password: "something",
+// };
+
+// function store() {
+//   const form = document.getElementById("signup");
+//   console.log(form);
+// }
